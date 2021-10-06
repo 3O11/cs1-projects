99c72d64f78926c310b2aec67cef7550c9c5f21f
Viktor Soukup <vitosoukup@gmail.com>
2021-10-06 23:29:21 +0200

    Implemented second assignment

---
+++ 02-WordFrequencyCounter/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace _02_WordFrequencyCounter {
+    class Program {
+        static void Main(string[] args) {
+            if (args.Length != 1) {
+                Console.WriteLine("Argument Error");
+                return;
+            }
+
+            if (!File.Exists(args[0])) {
+                Console.WriteLine("File Error");
+                return;
+            }
+
+            WordExtractor words = new WordExtractor(args[0]);
+            SortedDictionary<string, int> counter = new SortedDictionary<string, int>();
+            string word;
+            while ((word = words.GetNextRawWord()) != null) {
+                if (counter.ContainsKey(word)) {
+                    counter[word]++;
+                }
+                else {
+                    counter.Add(word, 1);
+                }
+            }
+            words.Dispose();
+
+            foreach(var pair in counter) {
+                Console.WriteLine(pair.Key + ": " + pair.Value);
+            }
+        }
+    }
+}
---
+++ 02-WordFrequencyCounter/WordExtractor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace _02_WordFrequencyCounter {
+    class WordExtractor : IDisposable {
+        public WordExtractor(string filename) {
+            _fileReader = new StreamReader(filename);
+        }
+
+        public void Dispose() {
+            _fileReader.Close();
+            _fileReader.Dispose();
+        }
+
+        public string GetNextRawWord() {
+            StringBuilder wordBuffer = new StringBuilder();
+            char next;
+
+            while (_fileReader.Peek() >= 0) {
+                next = (char)_fileReader.Read();
+                if (!char.IsWhiteSpace(next)) {
+                    wordBuffer.Append(next);
+                    break;
+                }
+            }
+
+            while (_fileReader.Peek() >= 0) {
+                next = (char)_fileReader.Read();
+
+                if (char.IsWhiteSpace(next)) break;
+
+                wordBuffer.Append(next);
+            }
+
+            if (wordBuffer.Length > 0) return wordBuffer.ToString();
+
+            return null;
+        }
+
+        StreamReader _fileReader;
+    }
+}

f75a12388f62f0e8facc1a569239fea9eaf1c26d
Viktor Soukup <vitosoukup@gmail.com>
2021-10-06 19:36:06 +0200

    Created Visual Studio solution, started first project

---
+++ WordCounter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace WordCounter {
+    class Program {
+        static void Main(string[] args) {
+            if (args.Length != 1) {
+                Console.WriteLine("Argument Error");
+                return;
+            }
+
+            if (!File.Exists(args[0])) {
+                Console.WriteLine("File Error");
+                return;
+            }
+
+            int counter = 0;
+            WordExtractor words = new WordExtractor(args[0]);
+            string word = words.GetNextRawWord();
+            while (word != null) {
+                counter++;
+                word = words.GetNextRawWord();
+            }
+            words.Dispose();
+
+            Console.WriteLine(counter);
+        }
+    }
+}
---
+++ WordCounter/WordExtractor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace WordCounter {
+    class WordExtractor : IDisposable {
+        public WordExtractor(string filename) {
+            _fileReader = new StreamReader(filename);
+        }
+
+        public void Dispose() {
+            _fileReader.Close();
+            _fileReader.Dispose();
+        }
+
+        public string GetNextRawWord() {
+            StringBuilder wordBuffer = new StringBuilder();
+            char next;
+
+            while (char.IsWhiteSpace(next = (char)_fileReader.Read()) && !_fileReader.EndOfStream);
+            if (!char.IsWhiteSpace(next)) wordBuffer.Append(next);
+            
+            while (!char.IsWhiteSpace(next = (char)_fileReader.Read()) && !_fileReader.EndOfStream) {
+                wordBuffer.Append(next);
+            }
+
+            if (wordBuffer.Length == 0) return null;
+
+            return wordBuffer.ToString();
+        }
+
+        StreamReader _fileReader;
+    }
+}

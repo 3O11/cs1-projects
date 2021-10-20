bbdb4997aba967a3310ab1c1a23aed4d4012cafc
Viktor Soukup <vitosoukup@gmail.com>
2021-10-20 14:41:12 +0200

    Reverted commenting change made to accomodate file limit in ReCodEx

--- 03-BlockAligner/BlockAligner.cs
+++ 03-BlockAligner/BlockAligner.cs
@@ -87,23 +87,4 @@ namespace _03_BlockAligner {
         bool _isSecondNewline = false;
         List<string> _wordBuffer = new List<string>();
     }
-
-    class FileBlockAligner : BlockAligner {
-        public FileBlockAligner(int lineLength, string filename) : base(lineLength) {
-            _out = new StreamWriter(filename);
-        }
-
-        public override void Finish() {
-            base.Finish();
-
-            _out.Close();
-            _out.Dispose();
-        }
-
-        protected override void saveLine(string line) {
-            _out.WriteLine(line);
-        }
-
-        StreamWriter _out;
-    }
 }

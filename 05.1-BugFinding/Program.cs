c4cd21a18a0b7024543abe6c9b74b469d9b63d30
Viktor Soukup <vitosoukup@gmail.com>
2021-11-04 12:21:22 +0100

    Fixed OHledaniBugu

---
+++ 05.1-BugFinding/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OHledaniBugu
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if (!long.TryParse(Console.ReadLine(), out long a) || a < 0)
+            {
+                Console.WriteLine("Error!");
+				return;
+            }
+
+			if (!long.TryParse(Console.ReadLine(), out long b) || b < 0)
+			{
+				Console.WriteLine("Error!");
+				return;
+			}
+
+			Console.WriteLine("Result: " + Math.Abs(a-b));	
+		}
+	}
+}
